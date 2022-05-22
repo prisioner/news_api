4a10959ab2e66a3653c9391eb7eccd2f2b2fda21
Aleksandr Klimenkov <prisioner@mail.ru>
2022-05-22 21:36:54 +0300

    authors list

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   has_many :articles, dependent: :destroy
   has_many :favorite_articles, dependent: :destroy
+
+  scope :authors, -> {
+    joins(:articles).where(articles: { published: true }).distinct
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :users, only: %i[] do
+        get :authors, on: :collection
+      end
+
       resources :articles, except: %i[edit new] do
         post :favorite, on: :member, action: :add_favorite
         delete :favorite, on: :member, action: :remove_favorite
